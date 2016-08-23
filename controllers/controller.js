6c5abe28ff8613737d105240a0b3c6b0a428a29a
Jorge Delgado <ajffdelgado@hotmail.com>
2016-08-22 23:24:22 -0300

    PArte 2 da aula

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,5 +2,26 @@ var app = angular.module('aula2', []);
 
 app.controller('Home', ['$rootScope', '$scope','$http', function($rootScope, $scope,$http){
 	
+	$scope.usuarios = [];
+	$scope.temCartao = "false";
+
+	$scope.mostrarCartao = function(mostrar){
+		if (mostrar == true){
+			$scope.temCartao = true;
+		} else{
+			$scope.temCartao = false;
+		}
+	}
+
+
+	$scope.cadastrar = function(){
+		alert($scope.sexo)
+		var usuario = {
+			nome:$scope.nome,
+			idade: $scope.idade,
+			sexo:$scope.sexo,
+			numero_cartao: $scope.numero
+		};
+	}
 }]);
 

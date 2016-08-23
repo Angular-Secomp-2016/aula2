35c78260438c5b63970cb37c07c6211f2112a895
Jorge Delgado <ajffdelgado@hotmail.com>
2016-08-22 23:42:17 -0300

    Fim da página index

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,11 +2,11 @@ var app = angular.module('aula2', []);
 
 app.controller('Home', ['$rootScope', '$scope','$http', function($rootScope, $scope,$http){
 	
-	$scope.usuarios = [];
-	$scope.temCartao = "false";
+	$rootScope.usuarios = [];
+	$scope.temCartao = false;
 
 	$scope.mostrarCartao = function(mostrar){
-		if (mostrar == true){
+		if (mostrar == "Sim"){
 			$scope.temCartao = true;
 		} else{
 			$scope.temCartao = false;
@@ -15,13 +15,19 @@ app.controller('Home', ['$rootScope', '$scope','$http', function($rootScope, $sc
 
 
 	$scope.cadastrar = function(){
-		alert($scope.sexo)
+	
 		var usuario = {
 			nome:$scope.nome,
 			idade: $scope.idade,
 			sexo:$scope.sexo,
-			numero_cartao: $scope.numero
+			numero_cartao: $scope.numero,
+			temCartao: $scope.possuiCartao
+
 		};
+
+		$rootScope.push(usuario);
+		window.location = "cadastrados.html"
+
 	}
 }]);
 
